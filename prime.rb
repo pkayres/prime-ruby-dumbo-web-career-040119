634eca5ae93a7b81f3a5f24910251a1a77f5e661
Pete A <peter.ayresmusic@gmail.com>
2019-03-10 04:12:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  else
+    flag = true
+    (2..(number - 1)).each do |n|
+       flag = false if number % n == 0
+    end
+  end
+ return flag
+end
